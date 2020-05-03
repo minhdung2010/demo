085c27362001e823d92841561c5d45ac09aaa382
Minh Dung <midutdt2010@gmail.com>
2020-05-03 08:03:54 +0700

    Danh sách phòng

---
+++ Room.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyQuanKaraoke.DTO
+{
+    public class Room
+    {
+        public Room(int id, string name, string status)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.Status = status;
+        } 
+
+        public Room(DataRow row)//hàm dựng xử lý dataRow
+        {
+            this.ID = (int)row["id"];
+            this.Name = row["name"].ToString();
+            this.Status = row["status"].ToString();
+        }
+
+        private string status;
+        private string name;
+        private int iD;
+
+        public int ID
+        {
+            get
+            {
+                return iD;
+            }
+
+            set
+            {
+                iD = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Status
+        {
+            get
+            {
+                return status;
+            }
+
+            set
+            {
+                status = value;
+            }
+        }
+    }
+}
