c3b94f5c3e8059799dc18bf6590b47b24127b3dc
Minh Dung <midutdt2010@gmail.com>
2020-05-03 08:02:21 +0700

    Hóa đơn

---
+++ Bill.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyQuanKaraoke.DTO //lưu trữ nhũng thuộc tính của Bill
+{
+    public class Bill
+    {
+        public Bill(int id, DateTime? dateCheckin, DateTime? dateCheckOut, int status,int discount = 0)
+        {
+            this.ID = id;
+            this.DateCheckIn = dateCheckin;
+            this.DateCheckOut = dateCheckOut;
+            this.Status = status;
+            this.Discount = discount;
+        }
+
+        public Bill(DataRow row)//lấy row
+        {
+            this.ID = (int)row["id"];
+            this.DateCheckIn = (DateTime?)row["dateCheckin"];
+         
+            
+            var dateCheckOutTemp = row["dateCheckOut"];
+            if(dateCheckOutTemp.ToString() != "")
+                this.DateCheckOut = (DateTime?)dateCheckOutTemp;
+
+
+            this.Status = (int)row["status"];
+
+            if(row["discount"].ToString() != "")
+                this.Discount = (int)row["discount"];
+        }
+
+        private int status;
+
+        private DateTime? dateCheckOut;
+
+        private DateTime? dateCheckIn;
+
+        private int iD;
+
+        private int discount;
+
+        public DateTime? DateCheckIn
+        {
+            get
+            {
+                return dateCheckIn;
+            }
+
+            set
+            {
+                dateCheckIn = value;
+            }
+        }
+
+        public int ID
+        {
+            get
+            {
+                return iD;
+            }
+
+            set
+            {
+                iD = value;
+            }
+        }
+
+        public DateTime? DateCheckOut
+        {
+            get
+            {
+                return dateCheckOut;
+            }
+
+            set
+            {
+                dateCheckOut = value;
+            }
+        }
+
+        public int Status
+        {
+            get
+            {
+                return status;
+            }
+
+            set
+            {
+                status = value;
+            }
+        }
+
+        public int Discount
+        {
+            get
+            {
+                return discount;
+            }
+
+            set
+            {
+                discount = value;
+            }
+        }
+    }
+}
