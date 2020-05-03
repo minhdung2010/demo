7818ef84f07b6407e5196d6ac972308674ecb229
Minh Dung <midutdt2010@gmail.com>
2020-05-03 08:03:15 +0700

    Đò ăn

---
+++ Food.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyQuanKaraoke.DTO
+{
+    public class Food
+    {
+        public Food(int id, string name, int categoryID, float price)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.CategoryID = categoryID;
+            this.Price = price;
+        }
+
+        public Food(DataRow row) 
+        {
+            this.ID = (int)row["id"];
+            this.Name = row["name"].ToString();
+            this.CategoryID = (int)row["idcategory"];
+            this.Price = (float)Convert.ToDouble(row["price"].ToString());
+        }
+
+        private float price;
+
+        private int categoryID;
+
+        private string name;
+
+        private int iD;
+
+        public int ID
+        {
+            get
+            {
+                return iD;
+            }
+
+            set
+            {
+                iD = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public int CategoryID
+        {
+            get
+            {
+                return categoryID;
+            }
+
+            set
+            {
+                categoryID = value;
+            }
+        }
+
+        public float Price
+        {
+            get
+            {
+                return price;
+            }
+
+            set
+            {
+                price = value;
+            }
+        }
+    }
+}
