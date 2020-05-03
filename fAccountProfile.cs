b6056486b3e9e6982de5be1fee73bf5a11be04bc
Minh Dung <midutdt2010@gmail.com>
2020-05-03 07:49:23 +0700

    Tài khoản

---
+++ fAccountProfile.cs
@@ -0,0 +1,86 @@
+﻿using QuanLyQuanKaraoke.DAO;
+using QuanLyQuanKaraoke.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyQuanKaraoke
+{
+    public partial class fAccountProfile : Form
+    {
+        private Account loginAccount;
+
+        public Account LoginAccount
+        {
+            get
+            {
+                return loginAccount;
+            }
+
+            set
+            {
+                loginAccount = value;
+                ChangeAccount(loginAccount);
+            }
+        }
+
+        public fAccountProfile(Account acc)
+        {
+            InitializeComponent();
+
+            LoginAccount = acc;
+        }
+
+        void ChangeAccount(Account acc)//show dữ liệu lên
+        {
+            txbUserName.Text = LoginAccount.UserName;
+            txbDisplayName.Text = LoginAccount.DisplayName; 
+        }
+
+        void UpdateAccount()
+        {   //truyền dữ liệu
+            string displayName = txbDisplayName.Text;
+            string password = txbPassWord.Text;
+            string newpass = txbNewPass.Text;
+            string reenterPass = txbReEnterPass.Text;
+            string userName = txbUserName.Text;
+
+            if (!newpass.Equals(reenterPass))
+            {
+                MessageBox.Show("Vui lòng nhập lại mặt khẩu đúng với mặt khẩu mới");
+            }
+            else
+            {
+                if (AccountDAO.Instance.UpdateAccount(userName, displayName, password, newpass))
+                {
+                    MessageBox.Show("Cập nhật thành công");
+                }
+                else
+                {
+                    MessageBox.Show("Vui lòng điền đúng mặt khẩu");
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            UpdateAccount();
+        }
+    }
+}
